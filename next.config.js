84f29faeb05a953efa9a38d8d708eb2cd80d70fa
Keenochetty <Keenochetty101@gmail.com>
2021-08-07 19:48:24 +0200

    update

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports ={
     images: {
         domains:["links.papareact.com"],
     },
+    env:{
+        mapbox_key:"pk.eyJ1Ijoia2Vlbm9jaGV0dHkxMDEiLCJhIjoiY2tzMXZzazJzMWc1aDMybzkwNmI2aG1qOCJ9.8M58ZbsJrY5Fgz5pysKagQ"
+    }
 };
